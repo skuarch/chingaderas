d2f1a1d5dd769ce1108fedd22b1f997975c150fd
Alfredo Bello <skuarch@yahoo.com.mx>
2018-03-08 17:02:44 -0600

    Update promtTheme.sh

--- promtTheme.sh
+++ promtTheme.sh
@@ -4,3 +4,8 @@ PS1="\n\[\033[1;33m\]files($(/bin/ls -1 | /usr/bin/wc -l | /bin/sed 's: ::g')) \
 
 // mac
 PS1="\n\[\033[1;33m\]files($(ls -1 | wc -l | sed 's: ::g')) \$(parse_git_branch)\[\033[00m\] \[$(tput bold)\]\$(/bin/pwd)\n\[$(tput setaf 2)\]-> \[\033[0m\]"
+
+// function
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
