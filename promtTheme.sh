5990e61c9f4b19e9c493163e1b64b8e9be3b1c6e
Alfredo Bello <skuarch@yahoo.com.mx>
2018-03-08 18:52:02 -0600

    Update promtTheme.sh

--- promtTheme.sh
+++ promtTheme.sh
@@ -6,7 +6,7 @@ PS1="\n\[\033[1;33m\]files($(/bin/ls -1 | /usr/bin/wc -l | /bin/sed 's: ::g')) \
 PS1="\n\[\033[1;33m\]files($(ls -1 | wc -l | sed 's: ::g')) \$(parse_git_branch)\[\033[00m\] \[$(tput bold)\]\$(/bin/pwd)\n\[$(tput setaf 2)\]-> \[\033[0m\]"
 
 // el completo
-PS1="\n\033[0;36m\]time[\T]\[\033[00m\] \033[0;35m\]history[\!]\[\033[00m\] \033[0;31m\]ram[11498636]\[\033[00m\]\n\033[0;33m\]\u@\h\[\033[00m\] \033[1;33m\]$(/bin/pwd)\[\033[00m\] \033[1;35m\]$(parse_git_branch)\[\033[00m\]\n$\[\]-> \[\033[0m\]"
+PS1="\n\033[0;36m\]time[\T]\[\033[00m\] \033[0;35m\]history[\!]\[\033[00m\] \033[0;31m\]ram[$(free_mem)]\[\033[00m\]\n\033[0;33m\]\u@\h\[\033[00m\] \033[1;33m\]$(/bin/pwd)\[\033[00m\] \033[1;35m\]$(parse_git_branch)\[\033[00m\]\n$\[\]-> \[\033[0m\]"
 
 // functions
 parse_git_branch() {
