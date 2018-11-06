35aa997f401cf680e5ad1dc4543394a9bf7b0428
Alfredo Bello <skuarch@yahoo.com.mx>
2018-11-05 21:59:23 -0600

    Update promtTheme.sh

--- promtTheme.sh
+++ promtTheme.sh
@@ -3,7 +3,7 @@ PS1="\n\[\033[1;33m\]files($(/bin/ls -1 | /usr/bin/wc -l | /bin/sed 's: ::g')) \
 PS1="\n\[\033[1;33m\]files($(/bin/ls -1 | /usr/bin/wc -l | /bin/sed 's: ::g')) \[\033[1;33m\]\$(/bin/ls -lah | /bin/grep -m 1 total | /bin/sed 's/total //') \[\033[38;5;95m\]\[$(tput bold)\]\$(/bin/pwd)\n\[$(tput setaf 2)\]-> \[\033[0m\]\[$(tput bold)"
 
 // mac
-PS1="\n\[\033[1;33m\]files($(ls -1 | wc -l | sed 's: ::g')) \$(parse_git_branch)\[\033[00m\] \[$(tput bold)\]\$(/bin/pwd)\n\[$(tput setaf 2)\]-> \[\033[0m\]"
+PS1='\n\[\033[1;31m\]$(date +%H:%M:%S) \[\033[1;33m\]$(pwd) \[\033[1;34m\]$(parse_git_branch) \n\[\033[1;35m\]-> \[\033[0m\] '
 
 // el completo
 PS1="\n\033[0;36m\]time[\T]\[\033[00m\] \033[0;35m\]history[\!]\[\033[00m\] \033[0;31m\]ram[\$(free_mem)]\[\033[00m\]\n\033[0;33m\]\u@\h\[\033[00m\] \033[1;33m\]\$(/bin/pwd)\[\033[00m\]\033[97m\]\$(parse_git_branch)\[\033[00m\]\n$\[\]-> \[\033[0m\]"
