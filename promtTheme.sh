75489cd18956c4133cf2bff574ac73b7aad39d02
Alfredo Bello <skuarch@yahoo.com.mx>
2020-03-29 18:41:20 -0600

    Update promtTheme.sh

--- promtTheme.sh
+++ promtTheme.sh
@@ -8,6 +8,9 @@ PS1='\n\[\033[1;31m\]$(date +%H:%M:%S) \[\033[1;33m\]$(pwd) \[\033[1;34m\]$(pars
 // el completo
 PS1="\n\033[0;36m\]time[\T]\[\033[00m\] \033[0;35m\]history[\!]\[\033[00m\] \033[0;31m\]ram[\$(free_mem)]\[\033[00m\]\n\033[0;33m\]\u@\h\[\033[00m\] \033[1;33m\]\$(/bin/pwd)\[\033[00m\]\033[97m\]\$(parse_git_branch)\[\033[00m\]\n$\[\]-> \[\033[0m\]"
 
+// sin fallas y completo
+PS1="\n\033[0;36m\][\$(/bin/pwd)]\[\033[00m\] \033[0;35m\][\W]\[\033[00m\] \033[0;32m\][\$(command_ok)]\[\033[00m\] \033[0;31m\][\$(get_governor)]\[\033[00m\] \033[0;33m\][\$(get_batery_percentage)]\[\033[00m\] \033[0;37m\][\$]\[\033[00m\]\033[0;32m\]\$(parse_git_branch)\[\033[00m\] \n-> "
+
 // functions
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
@@ -18,10 +21,18 @@ free_mem(){
 }
 
 command_ok(){
-    if [ $? = 0 ]; then
-        echo "✔";
+    if [ $? != 0 ]; then
+        echo "✘";        
     else
-        echo "✘";
+        echo "✔";
     fi
+}
+
+get_governor() {
+    cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+}
+
+get_batery_percentage(){
+    upower -i $(upower -e | grep 'BAT') | grep -E "state|to\ full|percentage" | awk '/perc/{print $2}'
 }
 
